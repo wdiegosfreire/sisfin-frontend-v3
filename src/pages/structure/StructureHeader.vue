34da44b4401e3c1cb2d1b88e2e208d9976d9658e
wdiegosfreire <wdiegosfreire@yahoo.com.br>
2025-10-23 16:03:41 -0300

    Updating project to VueJs 3.

--- src/pages/structure/StructureHeader.vue
+++ src/pages/structure/StructureHeader.vue
@@ -49,7 +49,7 @@
 
 					<v-list-item link @click="accessModuleAccount()" prepend-icon="mdi-bank-outline">Accounts</v-list-item>
 					<v-list-item link @click="accessModuleLocation()" prepend-icon="mdi-office-building-marker">Locations (OK)</v-list-item>
-					<v-list-item link @click="accessModulePaymentMethod()" prepend-icon="mdi-credit-card-check">Payment Methods</v-list-item>
+					<v-list-item link @click="accessModulePaymentMethod()" prepend-icon="mdi-credit-card-check">Payment Methods (OK)</v-list-item>
 					<v-list-item link @click="accessModuleBank()" prepend-icon="mdi-bank">Banks</v-list-item>
 					<v-list-item link @click="accessModuleStatementType()" prepend-icon="mdi-format-list-bulleted-type">Statement Types</v-list-item>
 					<v-list-item link @click="accessModuleStatementPattern()" prepend-icon="mdi-lock-pattern">Statement Patterns</v-list-item>
