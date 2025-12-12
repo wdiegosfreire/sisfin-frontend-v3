b0009686fe0864b12a8e3d07310d951a0b19c017
wdiegosfreire <wdiegosfreire@yahoo.com.br>
2025-12-11 21:21:25 -0300

    Adjustments for updating project to VueJs 3.

--- src/pages/structure/StructureHeader.vue
+++ src/pages/structure/StructureHeader.vue
@@ -37,8 +37,8 @@
 						<v-list-item v-bind="props">ACTIVITIES</v-list-item>
 					</template>
 
-					<v-list-item link @click="accessModuleSummary()" prepend-icon="mdi-text">Summary (OK)</v-list-item>
-					<v-list-item link @click="accessModuleObjective()" prepend-icon="mdi-cash-multiple">Objectives (OK)</v-list-item>
+					<v-list-item link @click="accessModuleSummary()" prepend-icon="mdi-text">Summary</v-list-item>
+					<v-list-item link @click="accessModuleObjective()" prepend-icon="mdi-cash-multiple">Objectives</v-list-item>
 					<v-list-item link @click="accessModuleStatement()" prepend-icon="mdi-notebook-plus">Statements</v-list-item>
 				</v-list-group>
 
@@ -47,12 +47,12 @@
 						<v-list-item v-bind="props">REGISTRATIONS</v-list-item>
 					</template>
 
-					<v-list-item link @click="accessModuleAccount()" prepend-icon="mdi-bank-outline">Accounts (OK)</v-list-item>
-					<v-list-item link @click="accessModuleLocation()" prepend-icon="mdi-office-building-marker">Locations (OK)</v-list-item>
-					<v-list-item link @click="accessModulePaymentMethod()" prepend-icon="mdi-credit-card-check">Payment Methods (OK)</v-list-item>
-					<v-list-item link @click="accessModuleBank()" prepend-icon="mdi-bank">Banks (OK)</v-list-item>
-					<v-list-item link @click="accessModuleStatementType()" prepend-icon="mdi-format-list-bulleted-type">Statement Types (OK)</v-list-item>
-					<v-list-item link @click="accessModuleStatementPattern()" prepend-icon="mdi-lock-pattern">Statement Patterns (OK)</v-list-item>
+					<v-list-item link @click="accessModuleAccount()" prepend-icon="mdi-bank-outline">Accounts</v-list-item>
+					<v-list-item link @click="accessModuleLocation()" prepend-icon="mdi-office-building-marker">Locations</v-list-item>
+					<v-list-item link @click="accessModulePaymentMethod()" prepend-icon="mdi-credit-card-check">Payment Methods</v-list-item>
+					<v-list-item link @click="accessModuleBank()" prepend-icon="mdi-bank">Banks</v-list-item>
+					<v-list-item link @click="accessModuleStatementType()" prepend-icon="mdi-format-list-bulleted-type">Statement Types</v-list-item>
+					<v-list-item link @click="accessModuleStatementPattern()" prepend-icon="mdi-lock-pattern">Statement Patterns</v-list-item>
 				</v-list-group>
 			</v-list-group>
 
