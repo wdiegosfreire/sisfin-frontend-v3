8ad8868e9c888a7ea1d910276cea39bb5439aa3b
wdiegosfreire <wdiegosfreire@yahoo.com.br>
2026-03-16 18:18:12 -0300

    Issue #11: Added new icons to statement registration form.

--- src/pages/transaction/account/AccountResult.vue
+++ src/pages/transaction/account/AccountResult.vue
@@ -2,7 +2,7 @@
 	<df-result-layout :collection-length="collection.length">
 		<v-treeview :items="collection" :item-title="traceAccountName" item-children="accountListChild" open-on-click>
 			<template v-slot:prepend="{ item }">
-				<v-icon v-if="item.icon" size="28" :icon="item.icon" />
+				<v-icon v-if="item.icon" size="28" :icon="item.icon" color="primary" />
 			</template>
 			<template v-slot:append="{ item }">
 				<div v-if="item.level.length > 3">
