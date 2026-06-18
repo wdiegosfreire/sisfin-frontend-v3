c2d917f8bac9ef73554fdac01ce9f14259ed50fe
wdiegosfreire <wdiegosfreire@yahoo.com.br>
2026-06-18 12:19:18 -0300

    Created v-skeleton-loader feature to result layout.

--- src/components/df-layout/ResultLayout.vue
+++ src/components/df-layout/ResultLayout.vue
@@ -1,14 +1,18 @@
 <template>
-	<v-card variant="elevated" elevation="3">
-		<v-card-text>
-			<h3 v-if="collectionLength > 0" class="text-left pb-3">Results in This Page: {{ collectionLength }}</h3>
-			<slot></slot>
-			<v-card-text v-if="collectionLength == 0">No results found.</v-card-text>
-		</v-card-text>
-	</v-card>
+	<v-skeleton-loader :loading="appStore.globalLoading" type="table" class="mx-auto border">
+		<v-card variant="elevated" elevation="3">
+			<v-card-text>
+				<h3 v-if="collectionLength > 0" class="text-left pb-3">Results in This Page: {{ collectionLength }}</h3>
+				<slot></slot>
+				<v-card-text v-if="collectionLength == 0">No results found.</v-card-text>
+			</v-card-text>
+		</v-card>
+	</v-skeleton-loader>
 </template>
 
 <script>
+import { useAppStore } from '@/stores/app';
+
 export default {
 	name: "DfResultLayout",
 
@@ -16,6 +20,12 @@ export default {
 		collectionLength: {
 			type: Number,
 			required: true
+		}
+	},
+
+	computed: {
+		appStore() {
+			return useAppStore();
 		}
 	}
 }
